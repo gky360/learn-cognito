d958696c8d1c2f3346015deaa81ee0fc7b71e427
gky360 <gky360@gmail.com>
2022-03-27 17:54:37 +0900

    Add Callback page

---
+++ frontend/pages/callback.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import styles from '../styles/Home.module.css';
+
+const Callback: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Callback Page</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Callback;
